0ff108fc46adeb18d16e6cd93be2204898f1e63d
Elizark-m <85119923+Elizark-m@users.noreply.github.com>
2022-03-25 13:40:25 +0400

    Update Chunk.cs

--- RimAlone/Chunk.cs
+++ RimAlone/Chunk.cs
@@ -8,11 +8,11 @@ namespace RimAlone
 {
     class Chunk
     {
-        //
+        // the number of cells from the center of the chunk not including the center
         private const int _chunkCellsRadius = 7;
         public int Radius { get { return _chunkCellsRadius; } }
         
-        //
+        // the number of cells icluded in the chunk
         private const int _chunkCellsCount = 170; 
         public int Count { get { return _chunkCellsCount; } }
         
@@ -20,23 +20,17 @@ namespace RimAlone
         private Coordinate _chunkCoordite;
         public Coordinate Coordinates { get { return _chunkCoordite; } }
 
-        //
+        // array of including cells
         private HexCell[] _Cells = new HexCell[_chunkCellsCount];
 
-        //6 layer:
-        //topLeft, topRight, centerLeft, centerRight, bottomLeft, bottomRight
+        // 6 layer:
+        // topLeft, topRight, centerLeft, centerRight, bottomLeft, bottomRight
         private HexCell[][] _RadiusCells = new HexCell[_chunkCellsCount][];
         
 
         public Chunk()
         {
             _chunkCoordite = new Coordinate();
-
-
         }
-
-
-
-
     }
 }
